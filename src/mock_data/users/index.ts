183509f410b200d8bfda1fc9b0bf13ab235ab9af
chenuvi <chenuvi@outlook.com>
2024-04-18 23:59:23 +0800

    Feat: route create

---
+++ src/mock_data/users/index.ts
@@ -0,0 +1,12 @@
+export const user_list = [
+  {
+    id: 1,
+    name: "chenuvi",
+    role: "ADMIN",
+  },
+  {
+    id: 2,
+    name: "p",
+    role: "INTERN",
+  },
+];
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,63 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from "@nestjs/common";
+import { user_list } from "src/mock_data/users";
+@Controller("users")
+export class UsersController {
+  /**
+   * Get /users
+   * Get /users/:id
+   * Post /users
+   * Patch /users
+   * Delete /users/:id
+   */
 
-@Controller('users')
-export class UsersController {}
+  @Get() // get users ir users?role=value
+  findAll(@Query("role") role?: "ADMIN" | "INTERN" | "ENGINEER") {
+    if (role) {
+      return user_list.filter((user) => user.role == role);
+    }
+    return user_list;
+  }
+
+  //   @Get("inters")
+  //   findAllInterns() {
+  //     return [];
+  //   }
+
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return {
+      id,
+      name: "id:" + id,
+    };
+  }
+
+  @Post()
+  create(@Body() users: {}) {
+    console.log("🚀 ~ UsersController ~ create ~ users:", users);
+    return users;
+  }
+
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() userInfo: {}) {
+    return {
+      id,
+      ...userInfo,
+    };
+  }
+
+  @Delete(":id")
+  delete(@Param("id") id: string) {
+    return {
+      id,
+    };
+  }
+}
